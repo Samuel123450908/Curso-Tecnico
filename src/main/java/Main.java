6b374713996a5c1e20e710ff231e40e95a58dbb3
Samuel Vitor Mariano <169068157+Samuel123450908@users.noreply.github.com>
2024-05-13 22:44:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("                            ---------------------------------");
+    System.out.println("                            Curso Desenvolvimento de sistemas");
+    System.out.println("                            ---------------------------------");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("   No começo desse curso eu achei que algo impossivel de aprender, mas com o passar do tempo percebi que não algo tão dificil assim, basta apenas ter vontade de aprender e querer estudar. Ate esse momento estou esse curso tecnico algo incrivel pois progamação e algo que eu sempre quis aprender. Ate o final desse ano espero estar dominando a linguagem java.");
+     
+    
+  }
+}
